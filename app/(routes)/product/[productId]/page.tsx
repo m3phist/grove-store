126183776191ad0752b668408a554885d23b77ff
Mephisto Aliff <aliff.ibr@icloud.com>
2023-08-08 17:05:30 +0800

    fixed product page

--- app/(routes)/product/[productId]/page.tsx
+++ app/(routes)/product/[productId]/page.tsx
@@ -17,7 +17,7 @@ const ProductPage: React.FC<ProductPageProps> = async ({ params }) => {
     categoryId: product?.category?.id,
   });
   return (
-    <div className="bg-white">
+    <div className="bg-white mt-12">
       <Container>
         <div className="px-4 py-4 sm:px-6 lg:px-8">
           <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8 ">
